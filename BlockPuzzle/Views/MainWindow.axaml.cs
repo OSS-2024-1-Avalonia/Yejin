04edaf71a2be573a9c139371267da302397fd4ca
pnudpwls1103 <pnudpwls1103@pusan.ac.kr>
2024-04-22 14:21:40 +0900

    First Commit

---
+++ BlockPuzzle/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace BlockPuzzle.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
